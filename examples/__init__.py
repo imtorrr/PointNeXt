3761de038c37d634bdfd5df9d88b970e379330cf
imtorrr <103179900+imtorrr@users.noreply.github.com>
2025-05-28 18:18:27 +0700

    Reformat code with ruff

--- examples/__init__.py
+++ examples/__init__.py
@@ -1,3 +1,4 @@
-import os, sys
-sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), '../'))
+import os
+import sys
 
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), "../"))
